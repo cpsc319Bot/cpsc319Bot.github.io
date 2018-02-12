0e2ac024017605aa11cf0f8f0949ae90efc54e96
cpsc319Bot <36424602+cpsc319Bot@users.noreply.github.com>
2018-02-12 14:23:58 -0800

    Update fill.js

--- fill.js
+++ fill.js
@@ -12,6 +12,23 @@ function querySt(ji) {
 }
 let firstName = querySt("first_name");
 let lastName = querySt("last_name");
+let riskLevel = querySt("riskLevel");
+let initialInvestment = querySt("initialInvestment");
+let regularAmount = querySt("regularAmount");
+let length = querySt("length");
+let interval = querySt("interval");
 
-document.getElementById('first_name').value = firstName;
-document.getElementById('last_name').value = lastName
+if (firstName)
+    document.getElementById('first_name').value = firstName;
+if (lastName)
+    document.getElementById('last_name').value = lastName;
+if(riskLevel)
+    document.getElementById('riskLevel').value = lastName;
+if(initialInvestment)
+    document.getElementById('initialInvestment').value = lastName;
+if(regularAmount)
+    document.getElementById('regularAmount').value = lastName;
+if(length)
+    document.getElementById('length').value = lastName;
+if(interval)
+    document.getElementById('interval').value = lastName;
